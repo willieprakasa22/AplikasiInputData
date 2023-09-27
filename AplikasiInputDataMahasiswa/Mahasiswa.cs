02be8342646bdfd883891c01d8f678dbd48204b1
willie prakasa <127827978+willieprakasa22@users.noreply.github.com>
2023-09-27 20:23:13 +0700

    Add files via upload

---
+++ AplikasiInputDataMahasiswa/Mahasiswa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiInputDataMahasiswa
+{
+    class Mahasiswa
+    {
+        public string Nim
+        {
+            get; set;
+        }
+
+        public string Nama
+        {
+            get; set;
+        }
+
+        public string Kelas
+        {
+            get; set;
+        }
+
+        public int Nilai
+        {
+            get; set;
+        }
+
+        public string Huruf;
+    }
+}
